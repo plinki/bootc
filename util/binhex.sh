825833640f88f3f6df18cb8b92224d78aef6ea88
plinki <geolazaria@gmail.com>
2023-02-28 20:31:16 +0200

    revert bin

--- util/binhex.sh
+++ util/binhex.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-input=$1
-xxd -p $input > bootsector.hex
+input="$1"
+bin_arg=false
+
+if [[ "$2" == "-b" || "$2" == "--bin" ]]; then
+    output="${input%.*}.bin"
+    xxd -r -p "$input" "$output"
+    echo "To bin"
+else
+    output="${input%.*}.hex"
+    xxd -p "$input" > "$output"
+fi

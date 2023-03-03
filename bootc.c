f0d08bd94301804ce83d34e30b88dea56540fd4a
plinki <geolazaria@gmail.com>
2023-03-03 19:12:00 +0200

    rewrite entry

--- bootc.c
+++ bootc.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "show.h"
 #include "bootsector.h"
 
 #define SECTOR_SIZE 512
 
+int argument_used(char* cmpr, char* arg, char* argument) {
+    return (strcmp(cmpr, arg) == 0 || strcmp(cmpr, argument) == 0);
+}
+
 int main(int argc, char* argv[]) {
-    BootSector bs;
-    bs.type = UNKNOWN;
+    enum Type bs_type = UNKNOWN;
+    int show_asm = 0;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+        if (argument_used(argv[i], "-h", "--help")) {
             printf("%s\n", "Usage: bootc [options] file");
             printf("%s\n", "Options:");
             printf("%s\n", "  -h, --help	   Print this message and exit");
             printf("%s\n", "  -t, --type TYPE  Set the boot sector type");
             printf("%s\n", "  -a, --asm		   Show assembly of the boot sector");
             return 0;
-        } else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--type") == 0) {
+        } else if (argument_used(argv[i], "-t", "--t")) {
             i++;
-            if (i < argc) {
-                char* type = argv[i];
+            char* type = argv[i];
 
-                if (strcmp(type, "pbr") == 0) {
-                    bs.type = PBR_FAT;
-                }
-
-                if (strcmp(type, "mbr") == 0) {
-                    bs.type = MBR;
-                }
-
+            if (strcmp(type, "pbr") == 0) {
+                bs_type = PBR_FAT;
             }
-        } else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--asm") == 0) {
-            // a
-            printf("%s\n", "a");
+
+            if (strcmp(type, "mbr") == 0) {
+                bs_type = MBR;
+            }
+        } else if (argument_used(argv[i], "-a", "--asm")) {
+            show_asm = 1;
         } else {
             FILE* input = fopen(argv[i], "rb");
             if (!input) {
-                printf("Error opening file %s\n", argv[1]);
+                printf("Error opening file %s\n", argv[i]);
                 return 1;
             }
 
@@ -48,33 +49,18 @@ int main(int argc, char* argv[]) {
                 return 1;
             }
 
-            do {
-                if (bs.type == UNKNOWN) {
-                    bs.type = infer(bs_buf);
-                }
-                if (bs.type == UNKNOWN) {
-                    printf("%s\n", "Unknown boot sector type.");
-                    return 1;
-                }
-            } while (0);
-
             fclose(input);
-            BootSector_init(&bs, bs_buf);
+            BootSector* bs = make_bs(bs_buf, infer(bs_buf));
+            BootSector_init(bs, bs_buf);
 
-            printf("%s%d\n", "Type: ", bs.type);
-            printf("%02x%02x\n", bs.data[510], bs.data[511]);
+            bs->type = (bs_type == UNKNOWN) ? infer(bs_buf) : bs_type;
 
-            /*
-            for (int i = 0; i < SECTOR_SIZE; i++) {
-            	printf("%s%02x ", "0x", bs.data[i]);
-            	if ((i+1) % 16 == 0) {
-            		printf("\n");
-               }
+            if (show_asm) {
+                print_info(bs->Pbr_bs);
             }
-            */
         }
-    }
 
+    }
     return 0;
 }
 
